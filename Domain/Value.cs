963c336cc0a44d9990537f64eca623d5ddf8c3d5
Youssef Younan <youssefunan966@outlook.com>
2020-09-15 16:12:35 +0200

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Data.Entity.Infrastructure;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Value : IDbAsyncEnumerable
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IDbAsyncEnumerator GetAsyncEnumerator()
+        {
+            return GetAsyncEnumerator();
+        }
+
+        
+    }
+}
---
+++ Presistance/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+
+
+namespace Presistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
